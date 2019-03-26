1fa9e4daa86a90f42e8b945d26119f56d4d1214a
Laurent Michel <laurent.michel@softozor.ch>
2019-03-26 17:15:40 +0100

    fix docker installation

--- scripts/InstallDocker.sh
+++ scripts/InstallDocker.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+# this move is necessary otherwise the installation asks whether we want to keep it
+mv /etc/init.d/kmod /etc/init.d/kmod.back
 apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 apt-key fingerprint 0EBFCD88
