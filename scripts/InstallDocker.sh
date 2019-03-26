8c60b9b502a6908058891fae7aac8df397419b78
Laurent Michel <laurent.michel@softozor.ch>
2019-03-26 16:36:33 +0100

    Update InstallDocker.sh
    auto yes in apt install

--- scripts/InstallDocker.sh
+++ scripts/InstallDocker.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 apt-key fingerprint 0EBFCD88
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt update
-apt install docker-ce docker-ce-cli containerd.io
+apt -y install docker-ce docker-ce-cli containerd.io
 service docker start
 usermod -aG docker jenkins
