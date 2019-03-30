72192b86037b4e96dbdebac94da81b08bae7c4d9
Laurent Michel <laurent.michel@softozor.ch>
2019-03-30 16:51:52 +0100

    fix job creation

---
+++ scripts/CreateJob.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+if [ $# -ne 1 ] ; then
+  echo "Usage: $0 <job-name>"
+fi
+
+USER=admin
+PASSWORD=$(cat /var/jenkins_home/secrets/initialAdminPassword)
+CLI="java -jar /var/jenkins_home/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ -auth $USER:$PASSWORD"
+
+JOB_NAME=$1
+
+cat ${JOB_NAME}.xml | $CLI create-job $JOB_NAME
